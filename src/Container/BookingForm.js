0af56be78bcd267a0dc9aaa3cd84051575a9b0b4
dilipbam <57613211+dilipbam@users.noreply.github.com>
2021-04-20 02:32:47 +0545

    implemented api for booking form

--- src/Container/BookingForm.js
+++ src/Container/BookingForm.js
@@ -1,105 +1,95 @@
-import {Component} from "react";
-import {Card, ListGroup, ListGroupItem} from 'reactstrap';
+import { get } from "jquery";
+import { Component } from "react";
+import { Card, ListGroup, ListGroupItem } from 'reactstrap';
+import axios from 'axios';
 
-class BookingForm extends Component{
-    render(){
-        return(
-            //cardview for booking truck
-            
-             // <!-- Page Content -->
-             <div className="Container">
+class BookingForm extends Component {
+    state = {
+        VehicleName : "",
+        VehicleNumber : "",
+        DrivingLicense : "",
+        DriverName : "",
+        Capacity : "",
+        Rate : "",
+        Type : "",
+        PickLocation: "",
+        DropLocation: "",
+        Phone: "",
+        Date: ""
+    }    
+    sendUserData = (e) =>{
+        e.preventDefault();
+        const data={
+            VehicleName : this.state.VehicleName,
+            VehicleNumber : this.state.VehicleNumber,
+            DrivingLicense : this.state.DrivingLicense,
+            DriverName : this.state.DriverName,
+            Capacity : this.state.Capacity,
+            Rate : this.state.Rate,
+            Type : this.state.Type,
+            PickLocation : this.state.PickLocation,
+            DropLocation : this.state.DropLocation,
+            Phone : this.state.Phone,
+            Data : this.state.Date
+        }
+        axios.post("http://localhost:90/getBooking", data)
+        .then(response=>{
+            console.log(response)
+        })
+        .catch(error=>{
+            console.log(error);
+        });
+    } 
+    render() {
+        return (
+            // <!-- Page Content -->
+            <div className="Container">
 
-             <h3 className="text-center booking-form-title">Hurry Up and rent a  best Car!!</h3>
+                <h3 className="text-center booking-form-title">Hurry Up and rent a  best Car!!</h3>
 
-             <div className="row booking-form-row">
-                 <div className="col-md-2"></div>
+                <div className="row booking-form-row">
+                    <div className="col-md-2"></div>
 
-                 <div className="col-md-8">
-                     <div className="card shadow mb-5 bg-white rounded booking">
-                     <Card style={{ width: '18rem' }}>
-                                    <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
-                                            <Card.Text>
-                                                Some quick example text to build on the card title and make up the bulk of
-                                                the card's content.
-                                            </Card.Text>
-                                    </Card.Body>
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroupItem>Cras justo odio</ListGroupItem>
-                                        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                                        <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                                    </ListGroup>
-                                    <Card.Body>
-                                        <Card.Link href="#">Book this Truck</Card.Link>
-                                    </Card.Body>
-                                </Card>
-                         <div className="card-body booking-form">
-                             <div className="row mt-4">
-                                 <div className="col-sm-6">
-                                     <span>PickUp Location</span>
-                                     <input type="text" name="pickUpLocation" className="form-control" placeholder="PickUp Location"
-                                         required="required" value={this.state.pickUpLocation} onChange={this.changeHandler}/>
-                                 </div>
-                                 <div className="col-sm-6">
-                                     <span>Return Location</span>
-                                     <input type="text" name="returnLocation" className="form-control" placeholder="Return Location"
-                                         required="required" value={this.state.returnLocation} onChange={this.changeHandler}/>
-                                 </div>
-                                 {/* <input type="submit" className="btn btn-primary" value="Set location" /> */}
-                             </div>
-
-                             <div className="row mt-4">
-
-                                 <div className="col-sm-6">
-                                     <span>PickUp Date</span>
-                                     <div className="input-icons">
-                                         <i className="fa fa-calendar icon"></i>
-                                         <input name="pickUpDate" className="input-field text-center form-control" type="date" 
-                                         id="daterange" size="35" value={this.state.pickUpDate} onChange={this.changeHandler}/>
-                                     </div>
-                                 </div>
-
-                                 <div className="col-sm-6">
-                                     <span>Return Date</span>
-                                     <div className="input-icons">
-                                         <i className="fa fa-calendar icon"></i>
-                                         <input name="returnDate" className="input-field text-center form-control" type="date" 
-                                         id="daterange" size="35" value={this.state.returnDate} onChange={this.changeHandler}/>
-                                     </div>
-                                 </div>
-                             </div>
-
-                             <div className="row mt-4">
-                                 <div className="col-sm-6">
-                                 <span>PickUp Time</span> 
-                                 <select name="pickUpTime" className="browser-default custom-select mb-4" id="select" value={this.state.pickUpTime} onChange={this.changeHandler}>
-                                     <option  disabled="" selected="">6:00 AM</option>
-                                     <option >7:00 AM</option>
-                                     <option >8:00 AM</option>
-                                     <option >9:00 AM</option>
-                                     <option >10:00 AM</option>
-                                     <option >11:00 AM</option>
-                                     <option >12:00 AM</option>
-                                 </select></div>
-                                 
-                                 <div className="col-sm-6">
-                                 <span>Return Time</span> 
-                                 <select name="returnTime" className="browser-default custom-select mb-4" id="select" value={this.state.returnTime} onChange={this.changeHandler}>
-                                     <option disabled="" selected="">6:00 AM</option>
-                                     <option >7:00 AM</option>
-                                     <option >8:00 AM</option>
-                                     <option >9:00 AM</option>
-                                     <option >10:00 AM</option>
-                                     <option >11:00 AM</option>
-                                     <option >12:00 AM</option>
-                                 </select></div>
-                             </div>
-                             <input type="submit" className="btn btn-primary btn-block" value="Book Now" onClick={this.book} />
-                         </div>
-                     </div>
-                 </div>
-             </div>
-         </div>            
+                    <div className="col-md-8">
+                        <div className="card shadow mb-5 bg-white rounded booking">
+                            {/* cardview for booking truck */}
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                </Card.Body>
+                                <ListGroup className="list-group-flush">
+                                    <ListGroupItem>{get.VehicleName}</ListGroupItem>
+                                    <ListGroupItem>{get.VehicleNumber}</ListGroupItem>
+                                    <ListGroupItem>{get.DrivingLicense}</ListGroupItem>
+                                    <ListGroupItem>{get.DriverName}</ListGroupItem>
+                                    <ListGroupItem>{get.Capacity}</ListGroupItem>
+                                    <ListGroupItem>{get.Rate}</ListGroupItem>
+                                    <ListGroupItem>{get.Type}</ListGroupItem>
+                                </ListGroup>                                
+                            </Card>
+                            <div className="login-form">
+                                <form action="" method="post">
+                                    <h4 className="modal-title">SignUp to create new Owner account</h4>
+                                    <div className="form-group">
+                                        <input type="text" name="pickUp" className="form-control" placeholder="Enter Goods PickUp Location" value={this.state.PickLocation} onChange={(event) => { this.setState({ PickLocation: event.target.value }) }} />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="dropLocation" className="form-control" placeholder="Enter Goods Delivery location" value={this.state.DropLocation} onChange={(event) => { this.setState({ DropLocation: event.target.value }) }} />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="phone" className="form-control" placeholder="Enter your Phone no." value={this.state.Phone} onChange={(event) => { this.setState({ Phone: event.target.value }) }} />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="date" name="date" className="form-control" placeholder="Choose date" value={this.state.Date} onChange={(event) => { this.setState({ Date: event.target.value }) }} />
+                                    </div>
+                                    <input type="submit" className="btn btn-primary btn-block btn-lg" value="SignUp" onClick={this.sendUserData} />
+                                </form>
+                            </div>
+                            <div className="col-md-4"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
